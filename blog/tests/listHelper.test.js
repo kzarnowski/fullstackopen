d8ef49acd730d1defeac7f96b8ed8ef77a444828
Krzysztof Zarnowski <kzarnowski7@gmail.com>
2024-02-10 08:38:08 +0100

    4a

--- blog/tests/listHelper.test.js
+++ blog/tests/listHelper.test.js
@@ -1,7 +1,32 @@
 const listHelper = require('../utils/list_helper')
+const mockBlogs = require('../mockBlogs.json')
 
 test('dummy returns one', () => {
   const blogs = []
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('totalLikes', () => {
+  test('returns 0 for empty array', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('returns correct sum', () => {
+    const result = listHelper.totalLikes(mockBlogs)
+    expect(result).toBe(34)
+  })
+})
+
+describe('favoriteBlog', () => {
+  test('returns undefined for empty array', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(undefined)
+  })
+
+  test('return favorite post', () => {
+    const result = listHelper.favoriteBlog(mockBlogs)
+    expect(result).toEqual(expect.objectContaining({ title: 'Canonical string reduction', likes: 12 }))
+  })
 })
--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
-  // ...
+  return 1
+}
+
+const totalLikes = (blogs) => blogs.reduce((likes, blog) => {return likes + blog.likes}, 0) ?? 0
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    return blogs.sort((a,b) => b.likes - a.likes)[0]
+  }
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
